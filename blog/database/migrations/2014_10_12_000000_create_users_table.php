3066a9dd5c082fff3f7648d7ff20cc87d4d62ff3
ldg <limdg5335@gmail.com>
2020-02-23 23:15:57 +0900

    update: 기존 프로젝트 삭제 및 laravel 프로젝트 업로드

---
+++ blog/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();  //이메일 중복되지 않도록 되어있음
+            $table->timestamp('email_verified_at')->nullable(); //nullable() : 공백으로 해도 된다.
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
