5acc039eec4c35eeba5786eed01be24674f9fc36
ldg <limdg5335@gmail.com>
2020-02-26 23:33:07 +0900

    new dashboard project init

---
+++ dashboard/note.js
@@ -0,0 +1,17 @@
+/**
+ * 개발스펙
+ * 
+ * os : windows10 pro 64bit
+ * php : 7.4.2
+ * composer: 1.9.3
+ * node: 13.2.0
+ * npm: 6.13.1
+ * laravel: 6.1.2
+ */
+
+ /**
+  *  0. 사전준비
+  *    0.1. laravel install
+  *      > composer create-project --prefer-dist laravel/laravel <프로젝트명>
+  *    0.2. npm install
+  */
